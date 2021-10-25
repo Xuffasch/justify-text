1c3556eb2646bfabe3e34dd56db78bfd1515f1f5
Xuffasch <tristan.lucas@gmail.com>
2021-10-26 00:29:37 +0200

    add api justify route

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+// Middleware to check provided accessToken
+const checkToken = require("../middlewares/authJwt.js");
+
 const apiController = require("../controllers/api.controller.js");
 
 router.post("/token", apiController.getToken);
+
+router.post("/justify", checkToken, apiController.justify);
 
 module.exports = router;
