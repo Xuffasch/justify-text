cb0825279c509529cc59b14573bd01b466445fcb
Xuffasch <tristan.lucas@gmail.com>
2021-10-26 00:29:13 +0200

    Add api justify controller function

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/user.model.js");
 const jwt = require("jsonwebtoken");
 
+const eightyChar = require("../services/eighty.js");
+
 exports.getToken = function (req, res) {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (err) {
@@ -30,4 +32,27 @@ exports.getToken = function (req, res) {
       });
     }
   });
+};
+
+exports.justify = function (req, res) {
+  if (!req.email) {
+    res.status(403).send({ message: "Unauthorized !" });
+  }
+
+  // Get all the words after cleaning up space characters and loose empty strings;
+  let words = req.body
+    .replace(/n/g, "")
+    .split(" ")
+    .filter((w) => w != "");
+
+  if (req.session && req.session.usage + words.length > 1000) {
+    res.status(403).send("Payment required - Daily usage limit reached ");
+  }
+
+  let output = eightyChar(req.body);
+  req.session.usage = !req.session.usage
+    ? words.length
+    : req.session.usage + words.length;
+
+  res.send(`${output}`);
 };
