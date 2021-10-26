d62163f6504be5c250ba1095cf740bf6c841293f
Xuffasch <tristan.lucas@gmail.com>
2021-10-26 11:44:07 +0200

    Set secure cookie

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(
     secret: "secretToProtectJustify",
     cookie: {
       httpOnly: true,
-      secure: false,
+      secure: process.env.ENVIRONMENT == "PRODUCTION" ? true : false,
       sameSite: false,
       maxAge:
         process.env.ENVIRONMENT == "PRODUCTION"
